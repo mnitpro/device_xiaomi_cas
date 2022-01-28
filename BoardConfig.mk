efe8ac300a8b275bd22251af6903f2502efd56cd
Ramii Ahmed <ramy@ahmedramy.com>
2022-01-28 18:00:51 +0200

    cas: BoardConfig: use LLVM tools

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -128,6 +128,7 @@ TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_ADDITIONAL_FLAGS := DTC_EXT=$(shell pwd)/prebuilts/misc/$(HOST_OS)-x86/dtc/dtc
 TARGET_KERNEL_SOURCE := kernel/xiaomi/cas
 TARGET_KERNEL_CONFIG := cas_defconfig
+KERNEL_SUPPORTS_LLVM_TOOLS := true
 
 # Metadata
 BOARD_USES_METADATA_PARTITION := true
