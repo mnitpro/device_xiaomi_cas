905eb7e4fbee411456ed842f5a55306471e48660
tarsin <43900799+asuka-mio@users.noreply.github.com>
2022-01-30 09:45:16 +0800

    cas: It's snow time!

--- evolution_cas.mk
+++ evolution_cas.mk
@@ -11,17 +11,20 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from the device configuration.
 $(call inherit-product, device/xiaomi/cas/device.mk)
 
-# Inherit from the ArrowOS configuration.
-$(call inherit-product, vendor/arrow/config/common.mk)
+# Inherit some common Evolution X stuff.
+$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
 
-PRODUCT_NAME := arrow_cas
+# Boot Animation
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Evolution-X things
+EVO_BUILD_TYPE := OFFICIAL
+EVO_MAINTAINER := tarsin
+
+PRODUCT_NAME := evolution_cas
 PRODUCT_DEVICE := cas
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := M2007J1SC
 PRODUCT_MANUFACTURER := Xiaomi
 
-DEVICE_MAINTAINER := Xayah
-
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
-BUILD_FINGERPRINT := Xiaomi/cas/cas:11/RKQ1.200826.002/V12.5.6.0.RJJCNXM:user/release-keys
