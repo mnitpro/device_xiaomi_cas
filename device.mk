77e4af44b504d815b208bd38939908c909466dcf
Alexander Koskovich <akoskovich@pm.me>
2022-06-02 12:22:05 +0800

    cas: Set the default IMS RCS package to com.android.service.ims
    Change-Id: Ib4a6f1763616418f6b8f02db5191abbae5ce9bbf

--- device.mk
+++ device.mk
@@ -331,7 +331,8 @@ PRODUCT_PACKAGES += \
     WifiResCommon \
     WifiResTarget \
     DeviceFrameworks \
-    DeviceSystemUI
+    DeviceSystemUI \
+    DeviceTelephony
 
 # Partitions
 PRODUCT_BUILD_SUPER_PARTITION := false
