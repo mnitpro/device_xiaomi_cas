0448bf588808302d26309317aa072d9c0d3365dc
Mahmoud Nabeel <mnitpro@gmail.com>
2022-06-09 16:24:18 +0300

    cas: update device for EvoX

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,18 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-<<<<<<< HEAD
-    $(LOCAL_DIR)/aosp_cas.mk
-
-COMMON_LUNCH_CHOICES := \
-    aosp_cas-eng \
-    aosp_cas-user \
-    aosp_cas-userdebug
-=======
     $(LOCAL_DIR)/evolution_cas.mk
 
 COMMON_LUNCH_CHOICES := \
     evolution_cas-eng \
     evolution_cas-user \
     evolution_cas-userdebug
->>>>>>> 905eb7e... cas: It's snow time!
