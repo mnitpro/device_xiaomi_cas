56d8127154dbfa268a2d0b24419615be4b8bf8d0
Mahmoud Nabeel <mnitpro@gmail.com>
2022-12-09 22:42:57 +0300

    Revert: cas: Update HALs

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,8 +1,8 @@
 echo 'Starting to clone stuffs needed for your device'
 # Stuff to remove
-rm -rf hardware/qcom-caf/sm8250/display
-rm -rf hardware/qcom-caf/sm8250/audio
-rm -rf hardware/qcom-caf/sm8250/media
+#rm -rf hardware/qcom-caf/sm8250/display
+#rm -rf hardware/qcom-caf/sm8250/audio
+#rm -rf hardware/qcom-caf/sm8250/media
 
 echo 'Cloning Device tree [1/7]'
 # Device Tree
@@ -24,10 +24,10 @@ echo 'Cloning display HAL [5/7]'
 # Display HALs
 git clone --depth=1 https://github.com/AcmeUI/android_hardware_qcom_display -b spring-caf-sm8250 hardware/qcom-caf/sm8250/display
 
-echo 'Cloning audio HAL [6/7]'
+#echo 'Cloning audio HAL [6/7]'
 # Audio HALs
-git clone --depth=1 https://github.com/AcmeUI/android_hardware_qcom_audio -b spring-caf-sm8250 hardware/qcom-caf/sm8250/audio
+#git clone --depth=1 https://github.com/AcmeUI/android_hardware_qcom_audio -b spring-caf-sm8250 hardware/qcom-caf/sm8250/audio
 
-echo 'Cloning media HAL [7/7]'
+#echo 'Cloning media HAL [7/7]'
 # Media HALs
-git clone --depth=1 https://github.com/AcmeUI/android_hardware_qcom_media -b spring-caf-sm8250 hardware/qcom-caf/sm8250/media
+#git clone --depth=1 https://github.com/AcmeUI/android_hardware_qcom_media -b spring-caf-sm8250 hardware/qcom-caf/sm8250/media
