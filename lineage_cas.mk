f4e76897ad383c5b8f85ac7de3280288b2d260a2
Mahmoud Nabeel <mnitpro@gmail.com>
2022-12-24 23:13:13 +0300

    cas: Lineage- ify

--- lineage_cas.mk
+++ lineage_cas.mk
@@ -11,26 +11,17 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from the device configuration.
 $(call inherit-product, device/xiaomi/cas/device.mk)
 
-# Inherit Evolution-X stuff.
-$(call inherit-product, vendor/evolution/config/common.mk)
+# Inherit from the lineageOS configuration.
+$(call inherit-product, vendor/lineage/config/common.mk)
 
-# GApps
-$(call inherit-product-if-exists, vendor/gapps/core/config.mk)
-
-PRODUCT_NAME := evolution_cas
+PRODUCT_NAME := lineage_cas
 PRODUCT_DEVICE := cas
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := M2007J1SC
 PRODUCT_MANUFACTURER := Xiaomi
-BOARD_VENDOR := Xiaomi
 
-# Boot animation
-TARGET_BOOT_ANIMATION_RES := 1080
-
-# FOD animations
-EXTRA_UDFPS_ANIMATIONS := true
+DEVICE_MAINTAINER := Xayah
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
 
 BUILD_FINGERPRINT := Xiaomi/cas/cas:12/RKQ1.211001.001/V13.0.1.0.SJJCNXM:user/release-keys
