7928301837195ffa50b42509b39ea51c661de8d7
Mahmoud Nabeel <mnitpro@gmail.com>
2023-01-21 23:43:40 +0300

    cas: unify prebuilt apps in a specific folder
    Moving all prebuilt apps (currently GCamGo and AdAway) to prebuilts folder

--- device.mk
+++ device.mk
@@ -373,6 +373,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti
 
+# Prebuilts
+PRODUCT_PACKAGES += \
+    adaway \
+    gcamgo
+
 # QMI
 TARGET_FWK_SUPPORTS_FULL_VALUEADDS := true
 
