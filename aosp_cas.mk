0905f2c6f7d5d8e26d654801838e0ac578448c6f
Mahmoud Nabeel <mnitpro@gmail.com>
2023-02-28 14:06:03 +0300

    cas: update blobs to 13.0.6

--- aosp_cas.mk
+++ aosp_cas.mk
@@ -41,4 +41,4 @@ PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-# BUILD_FINGERPRINT := Xiaomi/cas/cas:12/RKQ1.211001.001/V13.0.1.0.SJJCNXM:user/release-keys
+BUILD_FINGERPRINT := Xiaomi/cas/cas:12/RKQ1.211001.001/V13.0.6.0.SJJCNXM:user/release-keys
