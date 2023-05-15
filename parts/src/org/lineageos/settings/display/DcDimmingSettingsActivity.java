441b2724c3af64d60eb2b2b82d135bfa9dcad399
Zinger <mnitpro@gmail.com>
2023-05-15 07:11:21 +0300

    cas: Adapt XiaomiParts to taffy

--- parts/src/org/lineageos/settings/display/DcDimmingSettingsActivity.java
+++ parts/src/org/lineageos/settings/display/DcDimmingSettingsActivity.java
@@ -18,18 +18,16 @@
 package org.lineageos.settings.display;
 
 import android.os.Bundle;
+import android.preference.PreferenceActivity;
 
-import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
-import com.android.settingslib.widget.R;
-
-public class DisplaySettingsActivity extends CollapsingToolbarBaseActivity {
+public class DcDimmingSettingsActivity extends PreferenceActivity {
 
     private static final String TAG_DCDIMMING = "dcdimming";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getFragmentManager().beginTransaction().replace(R.id.content_frame,
-                new DisplaySettingsFragment(), TAG_DCDIMMING).commit();
+        getFragmentManager().beginTransaction().replace(android.R.id.content,
+                new DcDimmingSettingsFragment(), TAG_DCDIMMING).commit();
     }
 }
